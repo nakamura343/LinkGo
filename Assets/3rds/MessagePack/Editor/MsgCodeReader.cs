20a34b821c0bb741c874ea93feda00002bba2e0f
qiuyichen <qiuyichen.2014@gmail.com>
2019-12-26 21:16:33 +0800

    #014 整理结构

--- Assets/3rds/MessagePack/Editor/MsgCodeReader.cs
+++ Assets/3rds/MessagePack/Editor/MsgCodeReader.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace MessagePack.Editor
+namespace MessagePack
 {
     #region base classes
     public abstract class TypeBase
