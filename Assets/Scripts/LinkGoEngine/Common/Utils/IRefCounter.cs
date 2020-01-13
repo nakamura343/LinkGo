66d594871fb5582ba0f6607303960a334c9d8976
qiuyichen <qiuyichen.2014@gmail.com>
2020-01-13 16:59:21 +0800

    #添加公共引用计数类

---
+++ Assets/Scripts/LinkGoEngine/Common/Utils/IRefCounter.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// 公共引用计数接口类
+/// </summary>
+namespace LinkGo.Common.Utils
+{
+    public interface IRefCounter
+    {
+        int RefCount { get; }
+
+        void Retain();
+
+        void Release();
+    }
+}
---
+++ Assets/Scripts/LinkGoEngine/Common/Utils/SimpleRefCounter.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// 公共引用计数类
+/// </summary>
+namespace LinkGo.Common.Utils
+{
+    public class SimpleRefCounter : IRefCounter
+    {
+        public SimpleRefCounter()
+        {
+            RefCount = 0;
+        }
+
+        public int RefCount { get; private set; }
+
+        public void Retain()
+        {
+            ++RefCount;
+        }
+
+        public void Release()
+        {
+            --RefCount;
+            if (RefCount == 0)
+            {
+                OnZeroRef();
+            }
+        }
+
+        protected virtual void OnZeroRef()
+        {
+        }
+    }
+}
